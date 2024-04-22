da6954628ee318dd8e84221a25ad21fea0a93ba9
Jeff Geerling <geerlingguy@mac.com>
2024-04-22 15:46:18 -0500

    Show full iozone output still. PR #12 follow-up.

--- benchmarks/disk-benchmark.sh
+++ benchmarks/disk-benchmark.sh
@@ -100,7 +100,7 @@ printf "\n"
 # printf "\n"
 
 printf "Running iozone 4K / 1024K read and write tests...\n"
-./iozone -e -I -a -s 100M -r 4k -r 1024k -i 0 -i 1 -i 2 -f $DEVICE_MOUNT_PATH/iozone | cut -c7-78 | tail -n6 | head -n4
+./iozone -e -I -a -s 100M -r 4k -r 1024k -i 0 -i 1 -i 2 -f $DEVICE_MOUNT_PATH/iozone
 printf "\n"
 
 printf "Disk benchmark complete!\n\n"
